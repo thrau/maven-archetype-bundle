1034fd7a80bfa32ad8af01bccebc4ba579d14c23
Thomas Rausch <thomas@rauschig.org>
2013-05-10 15:40:36 +0200

    code import

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,17 @@
+package ${package};
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * Returns true!
+     */
+    public boolean isTrue() {
+        return true;
+    }
+}
